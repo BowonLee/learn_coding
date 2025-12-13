b8273adaa27eaa7143e81114ea616a417de29334
bowon lee <asdfg3678@gmail.com>
2025-12-14 00:43:36 +0900

    no message

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Process from './pages/Process';
 
 function App() {
     return (
-        <Router>
+        <Router basename={import.meta.env.BASE_URL}>
             <Routes>
                 <Route path="/" element={<Layout />}>
                     <Route index element={<Home />} />
